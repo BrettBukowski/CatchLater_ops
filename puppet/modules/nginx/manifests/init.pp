7b0b48aac75ab8bfd132215cbbcb3e26ddfe936e
Brett Bukowski <brett.bukowski@gmail.com>
2012-05-07 07:29:05 -0600

    More nginx

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -1,14 +1,21 @@
 class nginx {
   package {
     "nginx":
-      ensure => present
+      ensure => present,
+      before => File["/usr/local/nginx/nginx.conf"]
   }
   service {
     "nginx":
       ensure => true,
-      enable => true
+      enable => true,
+      subscribe => File["/usr/local/nginx/nginx.conf"],
   }
   file {
+    "/etc/init.d/nginx":
+      owner => root,
+      group => root,
+      mode => 755,
+      source => "puppet:///modules/nginx/"
     "/usr/local/nginx/nginx.conf":
       owner => root,
       group => root,
@@ -18,7 +25,9 @@ class nginx {
       owner => root,
       group => root,
       mode => 644,
-      source => "puppet:///modules/nginx/vhost";
+      source => "puppet:///modules/nginx/vhost",
+      notify => Service["nginx"],
+      require => Package["nginx"];
     "/user/local/nginx/sites-enabled/catchlater":
       ensure => 'link',
       target => '/usr/local/nginx/sites-available/catchlater';
